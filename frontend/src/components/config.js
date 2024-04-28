7e455c910643ef06d0afd0840b673ca6e0c90aca
Aakash-325 <aakashchaurasiya033@gmail.com>
2024-04-28 11:56:07 +0530

    endpoint change

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,3 +1,4 @@
-export const API_ENDPOINT = "http://localhost:8000";
+export const API_ENDPOINT = "https://ecommerce-e58n.onrender.com";
 
-export const STRIPE_PUBLIC_KEY = "pk_live_51N4yWFSDKMOeXJk0oGexjjiI9Yq74NPMZuyAx9Ec4HtjquelnW0KdZ5tsGPXHt4La5eBvOg0l1Z6aDh7pNmTSjAS00dG35ctkq";
+export const STRIPE_PUBLIC_KEY =
+  "pk_live_51N4yWFSDKMOeXJk0oGexjjiI9Yq74NPMZuyAx9Ec4HtjquelnW0KdZ5tsGPXHt4La5eBvOg0l1Z6aDh7pNmTSjAS00dG35ctkq";
